0850ba4580766b1e0f2f9967c8a3ea4d4da2004c
Ari <ari.lavigne@gmail.com>
2019-04-12 09:55:54 -0700

    Update Mediainfo_csv.rb

--- Mediainfo_csv.rb
+++ Mediainfo_csv.rb
@@ -50,7 +50,7 @@ fileinputs.each do |fileinput|
   $write_to_csv << @csvline
 end
 
-File.open(File.expand_path('~/Desktop/MediaInfo_.csv'), 'w') do |f|
+File.open(File.expand_path('~/Desktop/MediaInfo_CSV_#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}.csv'), 'w') do |f|
   f.puts "Name,Master File(gB),MP4(mb),DURATION"
    $write_to_csv.each do |line|
      f.puts(line)
