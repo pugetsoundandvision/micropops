fc05d34a6d58a0c8ed214055649dca2418c38e5f
A. Lavigne <32525121+alavigne12@users.noreply.github.com>
2019-04-23 15:14:35 -0700

    Delete Find_MP4s_Rsync.sh

--- Find_MP4s_Rsync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#while read TARGET
-#do rsync -av "$TARGET" [DESTINATION]
-#done < <(find "$PWD" -iname "*.mp4")
-
-while read TARGET
-do rsync -av "$TARGET" /Volumes/MAC2_DHD/PIs/WLM/00_Access
-done < <(find "$PWD" -iname "*.mp4")
