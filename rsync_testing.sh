045beb2a0f84ba7bde788a5ce9f9afa5b57fdbf2
A. Lavigne <32525121+alavigne12@users.noreply.github.com>
2019-04-23 15:15:08 -0700

    Delete rsync_testing.sh

--- rsync_testing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rsync -av mipopsmac1@10.5.65.121:/Users/mipopsmac1/Desktop/LTO_Test/Move_Here /Users/mipopsmac2/Desktop/NAS_Fake_1
-#sync directory from remote computer to local computer
-# cd /Users/mipopsmac2/Desktop/.  
-# pwd
-### <-- These two tests helped me determine that after entering the password, the expect script exits and returns to the LOCAL HOST.
-
