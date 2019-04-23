9d6a6067ef5c2d76ae558698b8b7f35e13caefd5
A. Lavigne <32525121+alavigne12@users.noreply.github.com>
2019-04-23 15:24:34 -0700

    Update Video_AIP_Loop.sh

--- Coding/VideoAIP/Video_AIP_Loop.sh
+++ Coding/VideoAIP/Video_AIP_Loop.sh
@@ -1,6 +1,8 @@
-#!/bin/bash #Loop for creating AIPS using VideoAIP
+#!/bin/bash 
+#Loop for creating AIPS using VideoAIP
+#This will only AIP MKVs.
 
-find /Users/mipopsmac2/Desktop/AIP_TEST -iname "*.mkv"
+find /PATH/TO/DIR -iname "*.mkv"
 while read TARGET
 do videoaip -l auto "$TARGET" </dev/null
-done < <(find /Users/mipopsmac2/Desktop/AIP_TEST -name '*.mkv')
+done < <(find /SAME/PATH/AS/ABOVE -name '*.mkv')
